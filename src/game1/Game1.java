035ec8a83d6318876b6ccee7a04e06d36ea1bf8b
Laura Barreto <labarreto@vassar.edu>
2014-10-11 11:14:00 -0700

    changes! so instead of an interface, using abstract class! :)

--- src/game1/Game1.java
+++ src/game1/Game1.java
@@ -14,85 +14,122 @@ import java.awt.Color;
 
 class Fishy {
     int eat;
-    Random rand = new Random();
-    int randNum = rand.nextInt();
+
     int x;
     int y;
     int width;
     int height;
+    
     int lives;
     int score;
+    
+    
     Posn pin;
     WorldImage fish;
     String fishFileName = new String("/Users/ldbruby95/NetBeansProjects/game1/fishy.png");
 
     public Fishy(Posn pin) {
-
         this.pin = pin;
         this.x = pin.x;
         this.y = pin.y;
+        this.lives = 3; //initial with 3 lives
+        this.score = 0; //initial score 0
         fish = new FromFileImage(pin, fishFileName);
         this.width = fish.getWidth();
         this.height = fish.getHeight();
-        this.lives = 3; //initial with 3 lives
-        this.score = 0; //initial score 0
+
     }
 
-    public int foodDistance(Food food) {
+    public int Distance(Nourishment nourishment) {
 
-        int diffX = this.x - food.pin.x;
-        int diffY = this.y - food.pin.x;
+        int diffX = this.x - nourishment.pin.x;
+        int diffY = this.y - nourishment.pin.x;
         int distance = (int) Math.sqrt((diffX * diffX) + (diffY * diffY));
         return distance;
 
     }
-
-    public int poisonDistance(Poison poison) {
-        int diffX = this.x - poison.pin.x;
-        int diffY = this.y - poison.pin.y;
-        int distance = (int) Math.sqrt((diffX * diffX) + (diffY * diffY));
-        return distance;
-    }   
+    
+    public boolean eatHuh(Nourishment nourishment) {
+        if (Distance(nourishment) < 5) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public void poisonedHuh(Nourishment nourishment) {
+        if (eatHuh(nourishment) && nourishment.isPoison()) {
+            this.lives--;
+        } else {
+            this.score++;
+        }
+    }
+    
+    public boolean isDeadHuh() {
+        if (this.lives == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     
 
-    
-    
-    
 
-
-    public Fishy moveFishy(String ke) {
-        if (ke.equals("right") && ((this.pin.x + this.width / 2) < 500)) {
+    public Fishy moveFishy(String ke) {  
+        //to never have out of bounds
+        if (ke.equals("right") && ((this.pin.x + this.width / 2) <= 495)) {
             return new Fishy(new Posn(x + 5, y));
-        } else if (ke.equals("left") && ((this.pin.x - this.width / 2) > 0)) {
+        } else if (ke.equals("left") && ((this.pin.x - this.width / 2) >= 5)) {
             return new Fishy(new Posn(x - 5, y));
-        } else if (ke.equals("up") && ((this.pin.y - this.height / 2) > 0)) {
+        } else if (ke.equals("up") && ((this.pin.y - this.height / 2) >= 5)) {
             return new Fishy(new Posn(x, y - 5));
-        } else if (ke.equals("down") && ((this.pin.y + this.height / 2) < 750)) {
+        } else if (ke.equals("down") && ((this.pin.y + this.height / 2) <= 745)) {
             return new Fishy(new Posn(x, y + 5));
         } else {
             return this;
         }
     }
 
+    public boolean outOfBounds(Fishy fish) {
+        //checking if the fish is out of bounds
+        if ((fish.pin.x < 495 && fish.pin.x > 5)
+                && (fish.pin.y < 745 && fish.pin.y > 5)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
     //setting fish
     public void setFishy(int x, int y) {
         this.pin = new Posn(x, y);
+
     }
 
 }
 
-interface Nourishments {
-
-    public boolean isPoison();
-}
+//interface Nourishments {
+//  JK, NOT USING THIS. 
+//    public boolean isPoison();
+//}
 
 abstract class Nourishment{
+    
     Nourishment nourishment;
-        
+    Posn pin = nourishment.pin;
+    int x = pin.x;
+    int y = pin.y;
+    abstract boolean isPoison();
+    
 }
 
 class Food extends Nourishment { // before i had implements Nourishments interface
                                   // cause of common isPoison method.
+    
+    
+    
+    //Random rand = new Random();
+    //int randNum = rand.nextInt();
     int eat = 5;
     public Posn pin;
     public int width;
@@ -149,6 +186,8 @@ class Poison extends Nourishment {
 
 }
 
+
+
 public class Game1 extends World {
 
     int WIDTH = 500;
@@ -194,6 +233,6 @@ public class Game1 extends World {
 
 //    public World onTick() {
 //        can't code anything yet because i want this to represent
-    //    the falling food blocks. 
+    //    the falling nourishments blocks. 
 //    }
 }

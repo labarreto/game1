f6ce793c0589f604cd68ea56c54ee5505257f6b8
Laura Barreto <labarreto@vassar.edu>
2014-10-13 01:39:18 -0400

    I’m kind of worried! SO MUCH TO DO, SO LITTLE TIME.

--- src/game1/Game1.java
+++ src/game1/Game1.java
@@ -13,20 +13,24 @@ import java.util.*;
 import java.awt.Color;
 
 class Fishy {
+
     int eat;
 
     int x;
     int y;
     int width;
     int height;
-    
+
     int lives;
     int score;
-    
-    
+
     Posn pin;
     WorldImage fish;
     String fishFileName = new String("/Users/ldbruby95/NetBeansProjects/game1/fishy.png");
+    int outBoundsRight = 500 - ((this.pin.x + this.width/2));
+    int outBoundsLeft = 0 + ((this.pin.x - this.width/2));
+    int outBoundsUp = 0 + ((this.pin.y = this.height/2));
+    int outBoundsDown = 750 - ((this.pin.y +this.height/2));
 
     public Fishy(Posn pin) {
         this.pin = pin;
@@ -48,7 +52,7 @@ class Fishy {
         return distance;
 
     }
-    
+
     public boolean eatHuh(Nourishment nourishment) {
         if (Distance(nourishment) < 5) {
             return true;
@@ -56,7 +60,7 @@ class Fishy {
             return false;
         }
     }
-    
+
     public void poisonedHuh(Nourishment nourishment) {
         if (eatHuh(nourishment) && nourishment.isPoison()) {
             this.lives--;
@@ -64,7 +68,7 @@ class Fishy {
             this.score++;
         }
     }
-    
+
     public boolean isDeadHuh() {
         if (this.lives == 0) {
             return true;
@@ -73,18 +77,30 @@ class Fishy {
         }
     }
     
+    public boolean isWinnerHuh(){
+        if (this.score == 100) {
+            return true;
+        } else {
+        return false;
+    }
+    }
 
-
-    public Fishy moveFishy(String ke) {  
+    public Fishy moveFishy(String ke) {
         //to never have out of bounds
-        if (ke.equals("right") && ((this.pin.x + this.width / 2) <= 495)) {
-            return new Fishy(new Posn(x + 5, y));
-        } else if (ke.equals("left") && ((this.pin.x - this.width / 2) >= 5)) {
-            return new Fishy(new Posn(x - 5, y));
-        } else if (ke.equals("up") && ((this.pin.y - this.height / 2) >= 5)) {
-            return new Fishy(new Posn(x, y - 5));
-        } else if (ke.equals("down") && ((this.pin.y + this.height / 2) <= 745)) {
-            return new Fishy(new Posn(x, y + 5));
+//        int outBoundsRight = 500 - ((this.pin.x + this.width/2));
+//        int outBoundsLeft = 0 + ((this.pin.x - this.width/2));
+//        int outBoundsUp = 0 + ((this.pin.y = this.height/2));
+//        int outBoundsDown = 750 - ((this.pin.y +this.height/2));
+        if (ke.equals("right") && ((this.pin.x + this.width / 2) <= outBoundsRight)) {
+            
+            return new Fishy(new Posn(x + 1, y));
+            
+        } else if (ke.equals("left") && ((this.pin.x - this.width / 2) >= outBoundsLeft)) {
+            return new Fishy(new Posn(x - 1, y));
+        } else if (ke.equals("up") && ((this.pin.y - this.height / 2) >= outBoundsUp)) {
+            return new Fishy(new Posn(x, y - 1));
+        } else if (ke.equals("down") && ((this.pin.y + this.height / 2) <= outBoundsDown)) {
+            return new Fishy(new Posn(x, y + 1));
         } else {
             return this;
         }
@@ -92,14 +108,14 @@ class Fishy {
 
     public boolean outOfBounds(Fishy fish) {
         //checking if the fish is out of bounds
-        if ((fish.pin.x < 495 && fish.pin.x > 5)
-                && (fish.pin.y < 745 && fish.pin.y > 5)) {
+        if ((fish.pin.x < outBoundsRight && fish.pin.x > outBoundsLeft)
+                && (fish.pin.y < outBoundsDown && fish.pin.y > outBoundsUp)) {
             return false;
         } else {
             return true;
         }
     }
-    
+
     //setting fish
     public void setFishy(int x, int y) {
         this.pin = new Posn(x, y);
@@ -112,36 +128,66 @@ class Fishy {
 //  JK, NOT USING THIS. 
 //    public boolean isPoison();
 //}
+abstract class Nourishment {
 
-abstract class Nourishment{
-    
     Nourishment nourishment;
     Posn pin = nourishment.pin;
     int x = pin.x;
     int y = pin.y;
+    Random rand = new Random();
+    int randNum = rand.nextInt();
+
     abstract boolean isPoison();
-    
+    abstract WorldImage nourishImage();
+
+    //set() --> void. sets position 
+    //x -> random 
+    public static boolean coinToss() {
+        Random randomInts = new Random();
+        int scene = randomInts.nextInt();
+        Math.abs(scene);
+        int remainder = scene % 2;
+        return (remainder == 1);
+    }
+
+    public static int randInt(int min, int max) {
+
+        Random rand = new Random();
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+        return randomNum;
+    }
+
+    public void set() {
+        x = randInt(0, 500);// random x value;
+        y = 0;
+        Posn pos = new Posn(x, y);
+
+        if (coinToss()) {
+            nourishment = new Food(pos);
+        } else {
+            nourishment = new Poison(pos);
+        }
+
+        //random x value;
+        // y always starts at 0. 
+        //  this.pin = new Posn(x,y);
+    }
+
 }
 
 class Food extends Nourishment { // before i had implements Nourishments interface
-                                  // cause of common isPoison method.
-    
-    
-    
+    // cause of common isPoison method.
+
     //Random rand = new Random();
     //int randNum = rand.nextInt();
-    int eat = 5;
-    public Posn pin;
+   // public Posn pin;
     public int width;
     public int height;
-    public int r;
     public IColor color = new Yellow();
     public WorldImage foodImage;
 
-    Random rand = new Random();
-    int randNum = rand.nextInt();
-    int x;
-    int y;
+    //int x;
+   // int y;
 
     WorldImage food;
 
@@ -154,25 +200,31 @@ class Food extends Nourishment { // before i had implements Nourishments interfa
 
     }
 
-    public WorldImage foodImage() {
+    public WorldImage nourishImage() {
 
         return new RectangleImage(this.pin, this.width, this.height, this.color);
+    }
+
+    public void setFood(int x, int y) {
+        x = randNum;
+        this.pin = new Posn(x, y);
     }
 
 }
 
 class Poison extends Nourishment {
 
-    public Posn pin;
+    
+//    public Posn pin;
+//    Random rand = new Random();
+//    int randNum = rand.nextInt();
+//    int x;
+//    int y;
+    
     public int width;
     public int height;
     public int r;
     public IColor color = new Red();
-
-    Random rand = new Random();
-    int randNum = rand.nextInt();
-    int x;
-    int y;
     public WorldImage poisonImage;
 
     public boolean isPoison() {
@@ -183,16 +235,26 @@ class Poison extends Nourishment {
 
         poisonImage = new RectangleImage(pin, width, height, color);
     }
+    
+    public WorldImage nourishImage() {
+        return new RectangleImage(this.pin, this.width, this.height, this.color);
+    }
+
+    public void setPoison(int x, int y) {
+        x = randNum;
+        this.pin = new Posn(x, y);
+    }
 
 }
-
-
 
 public class Game1 extends World {
 
     int WIDTH = 500;
     int HEIGHT = 750;
     Fishy fishy;
+    Nourishment nourishment;
+    int lives;
+    int score;
     int x = WIDTH / 2;
     int y = HEIGHT / 2;
     Posn center = new Posn(x, y);
@@ -201,9 +263,10 @@ public class Game1 extends World {
     String backFileName = new String("/Users/ldbruby95/NetBeansProjects/game1/tankback.png");
     WorldImage background;
 
-    public Game1(Fishy fishy) {// Food food, Poison poison
+    public Game1(Fishy fishy, Nourishment nourishment) {// Food food, Poison poison
         super();
         this.fishy = fishy;
+        this.nourishment = nourishment;
         background = new FromFileImage(center, backFileName);
 
     }
@@ -219,17 +282,42 @@ public class Game1 extends World {
 
     public WorldImage makeImage() {
         //overlaying fish image on the background image.
-        return new OverlayImages(this.fishy.fish, this.background);
+        WorldImage world = new OverlayImages(this.fishy.fish,
+                new OverlayImages(this.nourishment.nourishImage(), //want this to represent changing food
+                        new OverlayImages(
+                                new TextImage(new Posn(400, 20), "Lives:  " + lives, 20, new Black()),
+                                new OverlayImages(
+                                        new TextImage(new Posn(400, 40), "Score:  " + score, 20, new Black()), this.background))));
+
+        return world;
     }
 
     public World onKeyEvent(String ke) {
         if (ke.equals("q")) {
             return this.endOfWorld("Goodbye! See you later!");
         } else {
-            return new Game1(fishy.moveFishy(ke));
+            return new Game1(fishy.moveFishy(ke), nourishment);
         }
 
     }
+    
+    public World onTick() {
+        nourishment.set();
+        return new Game1(fishy, nourishment);
+    }
+    
+    public WorldEnd worldEnd() {
+        if (lives == 0) {
+            return new WorldEnd(true, 
+            new OverlayImages(background, 
+            new OverlayImages(new TextImage(new Posn(WIDTH/2, HEIGHT/2 - 30), "GAME OVER", 30, new Black()),
+            new TextImage(new Posn(WIDTH/2, HEIGHT/2), "Final Score: " + score, 20, new Black()))));
+        } else {
+            return new WorldEnd(false, this.makeImage());
+        }
+    }
+    
+    
 
 //    public World onTick() {
 //        can't code anything yet because i want this to represent
